c6f1ad4ce095d7395527e5989cce0ebed2b3e6d7
Jorge <jorge.plaza@usach.cl>
2019-06-06 21:38:52 -0400

    Subiendo lab3 Paradigmas

---
+++ Programa.pl
@@ -0,0 +1,575 @@
+stopword(a).    /*hechos para predicado stopword*/
+stopword(about). /*El dominio es un atomo*/
+stopword(above). /*Objetivo principal: Encontrar palabra definida como stopword*/
+stopword(across).
+stopword(after).
+stopword(afterwards).
+stopword(again).
+stopword(against).
+stopword(all).
+stopword(almost).
+stopword(alone).
+stopword(along).
+stopword(already).
+stopword(also).
+stopword(although).
+stopword(always).
+stopword(am).
+stopword(among).
+stopword(amongst).
+stopword(amoungst).
+stopword(amount).
+stopword(an).
+stopword(and).
+stopword(another).
+stopword(any).
+stopword(anyhow).
+stopword(anyone).
+stopword(anything).
+stopword(anyway).
+stopword(anywhere).
+stopword(are).
+stopword(around).
+stopword(as).
+stopword(at).
+stopword(back).
+stopword(be).
+stopword(became).
+stopword(because).
+stopword(become).
+stopword(becomes).
+stopword(becoming).
+stopword(been).
+stopword(before).
+stopword(beforehand).
+stopword(behind).
+stopword(being).
+stopword(below).
+stopword(beside).
+stopword(besides).
+stopword(between).
+stopword(beyond).
+stopword(bill).
+stopword(both).
+stopword(bottom).
+stopword(but).
+stopword(by).
+stopword(call).
+stopword(can).
+stopword(cannot).
+stopword(cant).
+stopword(co).
+stopword(computer).
+stopword(con).
+stopword(could).
+stopword(couldnt).
+stopword(cry).
+stopword(de).
+stopword(describe).
+stopword(detail).
+stopword(do).
+stopword(done).
+stopword(down).
+stopword(due).
+stopword(during).
+stopword(each).
+stopword(eg).
+stopword(eight).
+stopword(either).
+stopword(eleven).
+stopword(else).
+stopword(elsewhere).
+stopword(empty).
+stopword(enough).
+stopword(etc).
+stopword(even).
+stopword(ever).
+stopword(every).
+stopword(everyone).
+stopword(everything).
+stopword(everywhere).
+stopword(except).
+stopword(few).
+stopword(fifteen).
+stopword(fify).
+stopword(fill).
+stopword(find).
+stopword(fire).
+stopword(first).
+stopword(five).
+stopword(for).
+stopword(former).
+stopword(formerly).
+stopword(forty).
+stopword(found).
+stopword(four).
+stopword(from).
+stopword(front).
+stopword(full).
+stopword(further).
+stopword(get).
+stopword(give).
+stopword(go).
+stopword(had).
+stopword(has).
+stopword(hasnt).
+stopword(have).
+stopword(he).
+stopword(hence).
+stopword(her).
+stopword(here).
+stopword(hereafter).
+stopword(hereby).
+stopword(herein).
+stopword(hereupon).
+stopword(hers).
+stopword(herself).
+stopword(him).
+stopword(himself).
+stopword(his).
+stopword(how).
+stopword(however).
+stopword(hundred).
+stopword(i).
+stopword(ie).
+stopword(if).
+stopword(in).
+stopword(inc).
+stopword(indeed).
+stopword(interest).
+stopword(into).
+stopword(is).
+stopword(it).
+stopword(its).
+stopword(itself).
+stopword(keep).
+stopword(last).
+stopword(latter).
+stopword(latterly).
+stopword(least).
+stopword(less).
+stopword(ltd).
+stopword(made).
+stopword(many).
+stopword(may).
+stopword(me).
+stopword(meanwhile).
+stopword(might).
+stopword(mill).
+stopword(mine).
+stopword(more).
+stopword(moreover).
+stopword(most).
+stopword(mostly).
+stopword(move).
+stopword(much).
+stopword(must).
+stopword(my).
+stopword(myself).
+stopword(name).
+stopword(namely).
+stopword(neither).
+stopword(never).
+stopword(nevertheless).
+stopword(next).
+stopword(nine).
+stopword(no).
+stopword(nobody).
+stopword(none).
+stopword(noone).
+stopword(nor).
+stopword(not).
+stopword(nothing).
+stopword(now).
+stopword(nowhere).
+stopword(of).
+stopword(off).
+stopword(often).
+stopword(on).
+stopword(once).
+stopword(one).
+stopword(only).
+stopword(onto).
+stopword(or).
+stopword(other).
+stopword(others).
+stopword(otherwise).
+stopword(our).
+stopword(ours).
+stopword(ourselves).
+stopword(out).
+stopword(over).
+stopword(own).
+stopword(part).
+stopword(per).
+stopword(perhaps).
+stopword(please).
+stopword(put).
+stopword(rather).
+stopword(re).
+stopword(same).
+stopword(see).
+stopword(seem).
+stopword(seemed).
+stopword(seeming).
+stopword(seems).
+stopword(serious).
+stopword(several).
+stopword(she).
+stopword(should).
+stopword(show).
+stopword(side).
+stopword(since).
+stopword(sincere).
+stopword(six).
+stopword(sixty).
+stopword(so).
+stopword(some).
+stopword(somehow).
+stopword(someone).
+stopword(something).
+stopword(sometime).
+stopword(sometimes).
+stopword(somewhere).
+stopword(still).
+stopword(such).
+stopword(system).
+stopword(take).
+stopword(ten).
+stopword(than).
+stopword(that).
+stopword(the).
+stopword(their).
+stopword(them).
+stopword(themselves).
+stopword(then).
+stopword(thence).
+stopword(there).
+stopword(thereafter).
+stopword(thereby).
+stopword(therefore).
+stopword(therein).
+stopword(thereupon).
+stopword(these).
+stopword(they).
+stopword(thick).
+stopword(thin).
+stopword(third).
+stopword(this).
+stopword(those).
+stopword(though).
+stopword(three).
+stopword(through).
+stopword(throughout).
+stopword(thru).
+stopword(thus).
+stopword(to).
+stopword(together).
+stopword(too).
+stopword(top).
+stopword(toward).
+stopword(towards).
+stopword(twelve).
+stopword(twenty).
+stopword(two).
+stopword(un).
+stopword(under).
+stopword(until).
+stopword(up).
+stopword(upon).
+stopword(us).
+stopword(very).
+stopword(via).
+stopword(was).
+stopword(we).
+stopword(well).
+stopword(were).
+stopword(what).
+stopword(whatever).
+stopword(when).
+stopword(whence).
+stopword(whenever).
+stopword(where).
+stopword(whereafter).
+stopword(whereas).
+stopword(whereby).
+stopword(wherein).
+stopword(whereupon).
+stopword(wherever).
+stopword(whether).
+stopword(which).
+stopword(while).
+stopword(whither).
+stopword(who).
+stopword(whoever).
+stopword(whole).
+stopword(whom).
+stopword(whose).
+stopword(why).
+stopword(will).
+stopword(with).
+stopword(within).
+stopword(without).
+stopword(would).
+stopword(yet).
+stopword(you).
+stopword(your).
+stopword(yours).
+stopword(yourself).
+stopword(youselves).
+/*Hechos para predicado documento*/
+/*El dominio, es un entero (identificador) y dos strings (Titulo y texto)*/
+/*Objetivo principal: Encontrar el titulo y texto de un documento con cierto identificador determinado*/
+documento(1,"experimental investigation of the aerodynamics of a
+wing in a slipstream .", "experimental investigation of the aerodynamics of a
+wing in a slipstream .
+  an experimental study of a wing in a propeller slipstream was
+made in order to determine the spanwise distribution of the lift
+increase due to slipstream at different angles of attack of the wing
+and at different free stream to slipstream velocity ratios .  the
+results were intended in part as an evaluation basis for different
+theoretical treatments of this problem .
+  the comparative span loading curves, together with
+supporting evidence, showed that a substantial part of the lift increment
+produced by the slipstream was due to a /destalling/ or
+boundary-layer-control effect .  the integrated remaining lift
+increment, after subtracting this destalling lift, was found to agree
+well with a potential flow theory .
+  an empirical evaluation of the destalling effects was made for
+the specific configuration of the experiment .").
+documento(2,"simple shear flow past a flat plate in an incompressible fluid of small
+viscosity .", "simple shear flow past a flat plate in an incompressible fluid of small
+viscosity .
+in the study of high-speed viscous flow past a two-dimensional body it
+is usually necessary to consider a curved shock wave emitting from the
+nose or leading edge of the body .  consequently, there exists an
+inviscid rotational flow region between the shock wave and the boundary
+layer .  such a situation arises, for instance, in the study of the
+hypersonic viscous flow past a flat plate .  the situation is somewhat
+different from prandtl's classical boundary-layer problem . in prandtl's
+original problem the inviscid free stream outside the boundary layer is
+irrotational while in a hypersonic boundary-layer problem the inviscid
+free stream must be considered as rotational .  the possible effects of
+vorticity have been recently discussed by ferri and libby .  in the
+present paper, the simple shear flow past a flat plate in a fluid of small
+viscosity is investigated .  it can be shown that this problem can again
+be treated by the boundary-layer approximation, the only novel feature
+being that the free stream has a constant vorticity .  the discussion
+here is restricted to two-dimensional incompressible steady flow .").
+documento(3, "the boundary layer in simple shear flow past a flat plate .", "the boundary layer in simple shear flow past a flat plate .
+the boundary-layer equations are presented for steady
+incompressible flow with no pressure gradient .").
+documento(4, "approximate solutions of the incompressible laminar
+boundary layer equations for a plate in shear flow .", "approximate solutions of the incompressible laminar
+boundary layer equations for a plate in shear flow .
+  the two-dimensional steady boundary-layer
+problem for a flat plate in a
+shear flow of incompressible fluid is considered .
+solutions for the boundary-
+layer thickness, skin friction, and the velocity
+distribution in the boundary
+layer are obtained by the karman-pohlhausen
+technique .  comparison with
+the boundary layer of a uniform flow has also
+been made to show the effect of
+vorticity .").
+documento(5, "one-dimensional transient heat conduction into a double-layer
+slab subjected to a linear heat input for a small time", "one-dimensional transient heat conduction into a double-layer
+slab subjected to a linear heat input for a small time
+internal .
+  analytic solutions are presented for the transient heat
+conduction in composite slabs exposed at one surface to a
+triangular heat rate .  this type of heating rate may occur, for
+example, during aerodynamic heating .").
+/*Hechos para predicado indice*/
+/*El dominio es un atomo y una lista de integer*/
+/*Objetivo principal: Saber en que documentos, mediante su identificador, se encuentra la palabra*/
+indice(experimental, [1]).
+indice(investigation, [1]).
+indice(wing, [1]).
+indice(slipstream, [1]).
+indice(study, [1, 2]).
+indice(propeller, [1]).
+indice(made, [1, 4]).
+indice(order, [1]).
+indice(determine, [1]).
+indice(spanwise, [1]).
+indice(distribution, [1, 4]).
+indice(boundary, [1, 2, 3, 4]).
+indice(layer, [1, 2, 3, 4, 5]).
+indice(aerodynamics, [1]).
+indice(lift, [1]).
+indice(increase, [1]).
+indice(different, [1, 2]).
+indice(angles, [1]).
+indice(attack, [1]).
+indice(free, [1, 2]).
+indice(stream, [1, 2]).
+indice(velocity, [1, 4]).
+indice(ratios, [1]).
+indice(results, [1]).
+indice(intended, [1]).
+indice(part, [1, 2, 3]).
+indice(evaluation, [1]).
+indice(basis, [1]).
+indice(theoretical, [1]).
+indice(treatments, [1]).
+indice(problem, [1, 2, 4]).
+indice(comparative, [1]).
+indice(span, [1]).
+indice(loading, [1]).
+indice(curves, [1]).
+indice(together, [1]).
+indice(supporting, [1]).
+indice(evidence, [1]).
+indice(showed, [1]).
+indice(substancial, [1]).
+indice(increment, [1]).
+indice(produced, [1]).
+indice(control, [1]).
+indice(effect, [1, 2, 4]).
+indice(integrated, [1]).
+indice(remaining, [1]).
+indice(substracting, [1]).
+indice(destalling, [1]).
+indice(found, [1]).
+indice(agree,[1]).
+indice(well, [1]).
+indice(potential, [1]).
+indice(flow, [1, 2, 3, 4]).
+indice(theory, [1]).
+indice(empirical, [1]).
+indice(evaluation, [1]).
+indice(specific, [1]).
+indice(configuration, [1]).
+indice(simple, [2, 3]).
+indice(shear, [2, 3, 4]).
+indice(past, [2, 3]).
+indice(flat, [2, 3, 4]).
+indice(plate, [2, 3, 4]).
+indice(incompressible, [2, 3, 4]).
+indice(fluid, [2, 4]).
+indice(small, [2, 5]).
+indice(viscosity, [2]).
+indice(study, [1, 2]).
+indice(high, [2]).
+indice(speed, [2]).
+indice(viscous, [2]).
+indice(dimensional, [2, 4, 5]).
+indice(body, [2]).
+indice(usually, [2]).
+indice(necessary, [2]).
+indice(consider, [2, 4]).
+indice(curved, [2]).
+indice(shock, [2]).
+indice(wave, [2]).
+indice(emitting, [2]).
+indice(nose, [2]).
+indice(leading, [2]).
+indice(edge, [2]).
+indice(consequently, [2]).
+indice(exists, [2]).
+indice(inviscid, [2]).
+indice(rotational, [2]).
+indice(region, [2]).
+indice(situation, [2]).
+indice(arises, [2]).
+indice(instance, [2]).
+indice(hypersonic, [2]).
+indice(possible, [2]).
+indice(vorticity, [2, 4]).
+indice(discussed, [2]).
+indice(present, [2, 3, 5]).
+indice(paper, [2]).
+indice(treated, [2]).
+indice(approximation, [2]).
+indice(feature, [2]).
+indice(constant, [2]).
+indice(steady, [2, 3, 4]).
+indice(equations, [3, 4]).
+indice(pressure, [3]).
+indice(solutions, [4, 5]).
+indice(laminar, [4]).
+indice(thickness, [4]).
+indice(skin, [4]).
+indice(friction, [4]).
+indice(karman, [4]).
+indice(pohlhausen, [4]).
+indice(technique, [4]).
+indice(comparison, [4]).
+indice(uniform, [4]).
+indice(transient, [5]).
+indice(heat, [5]).
+indice(conduction, [5]).
+indice(double, [5]).
+indice(slab, [5]).
+indice(subjected, [5]).
+indice(linear, [5]).
+indice(input, [5]).
+indice(internal, [5]).
+indice(analytic, [5]).
+indice(composite, [5]).
+indice(exposed, [5]).
+indice(surface, [5]).
+indice(triangular, [5]).
+indice(rate, [5]).
+indice(occur, [5]).
+indice(example, [5]).
+indice(during, [5]).
+
+/*Predicado singleTermQuery*/
+/*Dominio: Un atomo y una lista de integer*/
+/*Objetivo principal: Saber en que documentos se encuentra la palabra*/
+singleTermQuery(Term, Result):- /*Clausula de Horn, Regla*/
+    not(stopword(Term)),
+    indice(Term, Result).
+
+/*Predicado bestMatch*/
+/*Dominio: Una lista de atomos y una lista de integer*/
+/*Objetivo principal: Saber en que documentos se encuentra la lista de palabras (atomos)*/
+bestMatch([],[]). /*Clausula de Horn, Hecho*/
+bestMatch([X|Xs] , Result):- /*Clausula de Horn, Regla*/
+    not(stopword(X)),
+    indice(X, Result);
+    bestMatch(Xs, Result).
+
+/*Predicado exactMatch*/
+/*Dominio: Una lista de atomos y una lista de integer*/
+/*Objetivo principal: Encontrar documentos con exactamente todas las palabras de la lista*/
+exactMatch([], []). /*Clausula de Horn, Hecho*/
+exactMatch([X|Xs], [D|Ds]):- /*Clausula de Horn, Regla*/
+    not(stopword(X)),
+    indice(X, D),
+    exactMatch(Xs, Ds).
+
+/*Predicado documents*/
+/*Dominio: Lista de integers y lista de strings*/
+/*Objetivo principal: Obtener titulo de los documentos en la lista*/
+documents([], []). /*Clausula de Horn, Hecho*/
+documents([X|Xs], [Y|Ys]):- /*Clausula de Horn, Regla*/
+    documento(X, Y, _),
+    documents(Xs, Ys).
+
+
+/*Predicado numDocuments*/
+/*Dominio: Una lista de documents (integer y 2 strings) y un integer*/
+/*Objetivo principal: Obtener la cantidad de documentos en la lista*/
+numDocuments([], 0). /*Clausula de Horn, Hecho*/
+numDocuments([_|Xs] , Count):- /*Clausula de Horn, Regla*/
+    numDocuments(Xs, Caux), Count is Caux+1.
+
+/*Predicado termTotalCount*/
+/*Dominio: Un atomo y un integer*/
+/*Objetivo principal: Obtener el numero de documentos en el que se encuentra la palabra*/
+termTotalCount(Term, Frequency):- /*Clausula de Horn, Regla*/
+    indice(Term, Lista), length(Lista, Frequency).
+
+/*Predicado maxFrequency*/
+/*Dominio: Un atomo*/
+/*Objetivo Principal: Obtener el termino con mayor numero de ocurrencia en documentos*/
+/*Ojetivo Secundario: Obtener el resto de terminos con mayor numero de ocurrencia en documentos*/
+maxFrequency(Term):- /*Clausula de Horn, Regla*/
+    indice(Term, Lista),length(Lista, 5).
+
+
+
